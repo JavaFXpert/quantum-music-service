cf75592238f29bc1ba6ca18640bf6f6261ce3787
JavaFXpert <james.l.weaver@gmail.com>
2018-02-23 15:21:02 -0500

    Prepare to have session with Ray Camden to refactor Vue components

--- static/piano.js
+++ static/piano.js
@@ -4,7 +4,8 @@ var soundpack_index=[1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.5,7,8,8.5,9,9.5,10,11,11.
 for(var i=0;i<soundpack_index.length;i++){
   soundpack.push({
     number: soundpack_index[i],
-    url: "https://awiclass.monoame.com/pianosound/set/"+ soundpack_index[i]+".wav"
+    //url: "https://awiclass.monoame.com/pianosound/set/"+ soundpack_index[i]+".wav"
+    url: "piano-sounds/"+ soundpack_index[i]+".wav"
   });
 
 }
